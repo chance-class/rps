f87d9096433d1632b029e267be63c967bc82813a
chance-class <129628753+chance-class@users.noreply.github.com>
2024-12-23 16:55:51 -0800

    Create pseudocode.md
    Add pseudocode to script.js as comments to organize code

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+
+// Create function getComputerChoice
+// Create option list(Rock, Paper, Scissors) 
+// Randomly select from option list for getComputerChoice
+// Create function getHumanChoice that will ask for input
+// Ensure input will match(case -insensitive) one of set options
+// Create variables computerScore and humanScore
+// Initialize variables at 0
+// Create function playRound
+// Compare computerChoice and humanChoice
+// Determine the winner of the round
+// Increment the winner's score
+// Console.log win / lose message
+// Create function playGame
+// Call playRound 5x
+// Declare winner of the game 
